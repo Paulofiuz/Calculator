90b1f9ec0b79b9d1cd2f3a87712e7a7640e520af
Eder Fiuza <paulofiuza24@gmail.com>
2024-06-11 21:13:11 -0300

    Adição de um menu no projeto.

--- Program.cs
+++ Program.cs
@@ -6,10 +6,34 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-
-
+            Menu();
+            
         }
 
+        static void Menu(){
+            Console.Clear();
+            Console.WriteLine("Welcome");
+            Console.WriteLine("Which operation is to perform?");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtraction");
+            Console.WriteLine("3 - Multiplication");
+            Console.WriteLine("4 - Division");
+            Console.WriteLine("5 - Exit the system");
+
+            Console.WriteLine("/-----------------------------------/");
+            Console.WriteLine("Select an option: ");
+            short SelectedNumber = short.Parse(Console.ReadLine());
+
+            switch(SelectedNumber){
+                case 1: Sum(); break;
+                case 2: Subtraction(); break;
+                case 3: Multiplication(); break;
+                case 4: Division();  break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+
+        }
         static void Sum()
         {
 
@@ -18,16 +42,17 @@ namespace Calculator
 
             float v1 = float.Parse(Console.ReadLine());
 
-
             Console.WriteLine("Second value: ");
             float v2 = float.Parse(Console.ReadLine());
 
-
             float result = v1 + v2;
             Console.WriteLine($"Result of the operation: {result}");
+
+            Console.ReadKey();
+            Menu();
         }
 
-        static void subtraction()
+        static void Subtraction()
         {
             Console.Clear();
             Console.WriteLine("Enter the first value: ");
@@ -36,14 +61,14 @@ namespace Calculator
             Console.WriteLine("Second value: ");
             float v2 = float.Parse(Console.ReadLine());
             
-            
-
             float result = v1 - v2;
             Console.WriteLine($"Result of the operation: {result}");
 
+             Console.ReadKey();
+            Menu();
         }
 
-        static void multiplication(){
+        static void Multiplication(){
             Console.Clear();
             Console.WriteLine("Enter the first value: ");
 
@@ -55,20 +80,24 @@ namespace Calculator
             float result = v1 * v2;
             Console.WriteLine($"Result of the operation: {result}");
 
+            Console.ReadKey();
+            Menu();
+            
         }
 
-        static void division(){
+        static void Division(){
             Console.WriteLine("Enter the first value: ");
 
             float v1 = float.Parse(Console.ReadLine());
 
-
             Console.WriteLine("Second value: ");
             float v2 = float.Parse(Console.ReadLine());
 
-
             float result = v1 / v2;
             Console.WriteLine($"Result of the operation: {result}");
+            
+            Console.ReadKey();
+            Menu();
         }
     }
 
